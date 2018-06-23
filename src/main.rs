8467d3d92128040b92fca45544a17842754e1aa5
niels <i.am@nilq.dk>
2018-06-23 13:45:56 +0200

    [lexer] Add lexer constructore

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,25 @@ extern crate colored;
 
 mod tang;
 use tang::lexer::*;
+use tang::source::*;
 
 fn main() {
-  println!("Hello, world!");
+  let content = r#"
+aw: int = 1000
+  "#;
+
+  let source = Source::from("<static.wu>", content.lines().map(|x| x.into()).collect::<Vec<String>>());
+  let lexer  = Lexer::default(content.chars().collect(), &source);
+
+  let mut tokens = Vec::new();
+
+  for token_result in lexer {
+    if let Ok(token) = token_result {
+      tokens.push(token)
+    } else {
+      return
+    }
+  }
+
+  println!("{:#?}", tokens)
 }
